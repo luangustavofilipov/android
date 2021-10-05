c090999f9cb4640e5c5d380a9d8752c55ce5d910
Luan Gustavo Altruda Filipov <luan.gustavo.filipov@gmail.com>
2021-10-04 21:01:28 -0300

    First Commit

---
+++ Aulas/Atividades/Atividade-1.py
@@ -0,0 +1,4 @@
+# Atividade 01 da wiki do professor Rodnei: http://www.pontelli.net.br/minhawiki/index.php/Programa%C3%A7%C3%A3o_para_Androd-01
+
+Msg = str(input("Escreva a mensagem: "))
+print(Msg)
---
+++ Aulas/Atividades/Atividade-2.py
@@ -0,0 +1,7 @@
+# Atividade 01 da wiki do professor Rodnei: http://www.pontelli.net.br/minhawiki/index.php/Programa%C3%A7%C3%A3o_para_Androd-01
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+Soma = n1 + n2
+
+print("A soma dos dois valores é: ", Soma)
