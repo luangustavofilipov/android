bddbaa7adbec2698a91eb477d19e18f1c6f6a729
Luan Gustavo Altruda Filipov <luan.gustavo.filipov@gmail.com>
2021-10-07 09:02:36 -0300

    Variables added

--- pizza.py
+++ pizza.py
@@ -1,1 +1,18 @@
 # Questionáro Inicial
+ValorPizzaPadrao = 27.90
+ValorPizzaRefri = 27.90 + 9.20
+MsgErro = "ERRO!"
+#Opcao = str(input("Digite a opção: "))
+#InfoEmail = str(input("Digite seu e-mail ou hotmail: "))
+#InfoTelCel = str(input("Digite seu telefone ou celular: "))
+InfoNome = str(input("Digite seu nome: "))
+#CalculoSoma = 
+Nome = InfoNome
+
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("Bem vindo!")
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("Olá",Nome)
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
